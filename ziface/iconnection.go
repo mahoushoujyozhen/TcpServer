5650d6f8de708e6f47eae797894f0c9311e688f6
mahoushoujyoZhen <389403710@qq.com>
2023-08-13 17:15:40 +0800

    将拆包封包集成到框架中，connection提供向客户端发包的接口

--- ziface/iconnection.go
+++ ziface/iconnection.go
@@ -8,8 +8,13 @@ type IConnection interface {
 	//停止连接，结束当前的连接状态
 	Stop()
 	//从当前连接获取原始的socket TCPConn GetTCOConnection() *net.TCPConn
+	GetTCPConnection() *net.TCPConn
 	// 获取当前连接的ID
 	GetConnID() uint32
+	//获取远程客户端地址信息
+	RemoteAddr() net.Addr
+	//直接将Message数据发送数据给远程的TCP客户端
+	SendMsg(msgId uint32, data []byte) error
 }
 
 // 定义一个统一处理链路业务的接口
--- znet/requset.go
+++ znet/requset.go
@@ -1,10 +1,13 @@
 package znet
 
-import "TcpServer/ziface"
+import (
+	"TcpServer/ziface"
+	"TcpServer/znet"
+)
 
 type Request struct {
 	conn ziface.IConnection // 已经和客户端建立好的 连接
-	data []byte             //客户端请求的数据
+	msg ziface.IMessage           //客户端请求的数据
 }
 
 //获取请求连接信息
@@ -12,6 +15,13 @@ type Request struct {
 func (r *Request) GetConnection() ziface.IConnection {
 	return r.conn
 }
+
+//获取请求消息的数据
 func (r *Request) GetData() []byte {
-	return r.data
+	return r.msg.GetData()
+}
+
+//获取请求消息的ID
+func (r *Request) GetMsgId()uint32{
+	return r.msg.GetMsgId()
 }
