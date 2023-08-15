51574a26fb667b1b66bb7cc10fbda29da483c420
mahoushoujyoZhen <389403710@qq.com>
2023-08-15 18:10:11 +0800

    增加消息队列多任务结构

--- utils/globalobj.go
+++ utils/globalobj.go
@@ -12,14 +12,27 @@ import (
 */
 
 type GlobalObj struct {
+	/*
+		Server
+	*/
 	TcpServer ziface.IServer //当前Zinx的全局Server对象
 	Host      string         //当前服务器主机IP
-	TcpPort   int            //当前服务器主机监听的端口
-	Name      string         // 当前服务器名称
-	Version   string         // 当前Zinx版本号
+	TcpPort   int            //当前服务器主机监听端口号
+	Name      string         //当前服务器名称
 
-	MaxPacketSize uint32 //所需数据包的最大值
-	MaxConn       int    // 当前服务主机允许最大的连接数
+	/*
+		Zinx
+	*/
+	Version          string //当前Zinx版本号
+	MaxPacketSize    uint32 //都需数据包的最大值
+	MaxConn          int    //当前服务器主机允许的最大链接个数
+	WorkerPoolSize   uint32 //业务工作Worker池的数量
+	MaxWorkerTaskLen uint32 //业务工作一个Worker对应负责的任务队列最大任务存储数量
+
+	/*
+		config file path
+	*/
+	ConfFilePath string
 }
 
 /*
@@ -53,14 +66,17 @@ func (g *GlobalObj) Reload() {
 */
 
 func init() {
-	//初始化GlobalObj变量，设置一些默认值
+	//初始化GlobalObject变量，设置一些默认值
 	GlobalObject = &GlobalObj{
-		Name:          "TcpServer",
+		Name:          "ZinxServerApp",
 		Version:       "V0.4",
 		TcpPort:       7777,
 		Host:          "0.0.0.0",
 		MaxConn:       12000,
 		MaxPacketSize: 4096,
+		ConfFilePath:  "conf/zinx.json",
+		WorkerPoolSize: 10,
+		MaxWorkerTaskLen: 1024,
 	}
 	//从配置文件中加载一些用户配置的参数
 	GlobalObject.Reload()
--- ziface/imsghandler.go
+++ ziface/imsghandler.go
@@ -2,9 +2,10 @@ package ziface
 
 /*
 	消息管理抽象层
-*/	
-type IMsgHandle interface{
-	DoMsgHandler (request IRequest)  //马上以非阻塞方式处理消息  调用Router中具体Handle()接口
-	AddRouter(msgId uint32,router IRouter)   //为消息添加具体的处理逻辑  添加一个msgId和一个路由关系到Apis中
+*/
+type IMsgHandle interface {
+	DoMsgHandler(request IRequest)          //马上以非阻塞方式处理消息  调用Router中具体Handle()接口
+	AddRouter(msgId uint32, router IRouter) //为消息添加具体的处理逻辑  添加一个msgId和一个路由关系到Apis中
+	StartWorkerPool()                       //启动worker工作池
+	SendMsgToTaskQueue(request IRequest)    //将消息交给TaskQueue，由worker进行处理
 }
-
